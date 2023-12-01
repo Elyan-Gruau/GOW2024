47cc56b3fed4d9166faea3c5bdac432a79bab8f0
Elyan-Gruau <elyangruau.pro@gmail.com>
2023-12-01 13:39:15 +0100

    Setup du canvas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as BABYLON from '@babylonjs/core';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('renderCanvas');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const engine = new BABYLON.Engine(canvas);
+
+const createScne = function(){
+    const scene = new BABYLON.Scene(engine);
+    return scene;
+}
+
+const scene = createScne();
+
+engine.runRenderLoop(function (){
+    scene.render();
+});
