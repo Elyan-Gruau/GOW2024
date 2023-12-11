a1152edf3bbc726d258375f2a99f8f51138f3992
Elyan-Gruau <elyangruau.pro@gmail.com>
2023-12-11 13:29:29 +0100

    Ajout de l'environement

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,43 @@ import * as BABYLON from '@babylonjs/core';
 
 const canvas = document.getElementById('renderCanvas');
 
+// @ts-ignore
 const engine = new BABYLON.Engine(canvas);
 
-const createScne = function(){
+interface BoxMesh extends BABYLON.Mesh {
+
+}
+
+interface GroundMesh extends BABYLON.Mesh {
+
+}
+
+const createScene = function(){
     const scene = new BABYLON.Scene(engine);
+    scene.createDefaultCameraOrLight(true, false,true);
+    //const box = new BABYLON.MeshBuilder.CreateBox();
+
+    const ground : GroundMesh =  BABYLON.MeshBuilder.CreateGround('ground',{
+       height : 10,
+       width : 10,
+       subdivisions : 30
+    });
+
+    ground.material = new BABYLON.StandardMaterial();
+    ground.material.wireframe = true;
+
+    const box : BoxMesh = BABYLON.MeshBuilder.CreateBox("myBox", {
+        size : 0.1
+    }, scene);
     return scene;
 }
 
-const scene = createScne();
+const scene = createScene();
 
 engine.runRenderLoop(function (){
     scene.render();
+});
+
+window.addEventListener('resize', function(){
+   engine.resize() ;
 });
