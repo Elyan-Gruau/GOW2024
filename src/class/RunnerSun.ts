9c647c19d8fd8c17fecfb0231bf611ed3de597fe
Elyan-Gruau <elyangruau.pro@gmail.com>
2024-04-30 11:30:22 +0200

    Réglage de l'éclairage + materiaux

--- src/class/RunnerSun.ts
+++ src/class/RunnerSun.ts
@@ -1,35 +1,44 @@
-import {MeshBuilder, Scene} from "@babylonjs/core";
+import {Color3, MeshBuilder, Scene, Vector3} from "@babylonjs/core";
 import * as BABYLON from "@babylonjs/core";
 
 export class RunnerSun {
 
-    private lampLight: BABYLON.Light;
-    private sunBall ;
+    private directionalLight: BABYLON.Light;
+    private ambiantLight: BABYLON.Light;
+
+
     private color: BABYLON.Color3;
     private intensity: number;
     private maxIntesity : number;
+    private minIntensity : number;
     private scene : Scene;
+    private sunSate:number;
 
     constructor(scene: Scene) {
         // Lamp
         const position: BABYLON.Vector3 = new BABYLON.Vector3(0, 1, 0);
         const direction : BABYLON.Vector3 = new BABYLON.Vector3(-10.2, -10, 10);
 
-        this.sunBall = MeshBuilder.CreateSphere('sun', {
-            diameter: 0.1,
-          }, scene);
-        this.sunBall.position = position;
 
-        this.lampLight = new BABYLON.DirectionalLight("lampLight",direction, scene);
-        this.color = new BABYLON.Color3(255, 255,250); // Couleur de départ (blanc)
-        this.intensity = 0.1; // Intensité de départ
+        //Ambiant light
+        this.ambiantLight = new BABYLON.DirectionalLight("ambiantLightSun",new Vector3(0,-1,0),scene);
+        this.ambiantLight.diffuse =new Color3(125,125,125);
+        this.ambiantLight.intensity = 0.31;
+
+        //Direcitonal light
+        this.directionalLight = new BABYLON.DirectionalLight("directionalLight",direction, scene);
+        this.color = new BABYLON.Color3(125, 125,125); // Couleur de départ (blanc)
+        this.intensity = 1; // Intensité de départ
         this.maxIntesity = this.intensity;
+        this.minIntensity = 0.0
 
-        this.lampLight.diffuse = this.color;
-        this.lampLight.intensity = this.intensity;
+        this.directionalLight.diffuse = this.color;
+        this.directionalLight.intensity = this.intensity;
 
-        const positionGizmo = new BABYLON.PositionGizmo();
-        positionGizmo.attachedMesh = this.lampLight;
+
+        //
+        // const positionGizmo = new BABYLON.PositionGizmo();
+        // positionGizmo.attachedMesh = this.directionalLight;
 
 
 
@@ -39,15 +48,27 @@ export class RunnerSun {
     // Mettez à jour le cycle jour/nuit
     update(deltaTime: number) {
         // Exemple : Cycle jour/nuit toutes les 10 secondes
-        const cycleDuration = 10; // en secondes
-        const cycleProgress = (Math.sin((deltaTime / cycleDuration) * Math.PI * 2) + 1) / 2;
+        const cycleDuration = 3; // en secondes
+        // const cycleProgress = (Math.sin((deltaTime / cycleDuration) * Math.PI * 2) + 1) / 2;
+        const cycleProgress = (Math.sin((deltaTime / cycleDuration) )*1000);
+
+
+        //TODO trouver un moyen de faire varier l'intensité
+        if (this.sunSate>=20){
+            this.directionalLight.diffuse = this.color;
+            this.directionalLight.intensity = this.intensity;
+        }else{
+            this.sunSate += deltaTime;
+        }
+
         //
         // // Modifiez la couleur et l'intensité en fonction du cycle jour/nuit
-        this.color = BABYLON.Color3.Lerp( this.color, new BABYLON.Color3(0.0, 0.2, 0.2), cycleProgress);
-        this.intensity = BABYLON.Scalar.Lerp(this.maxIntesity, 0.5, cycleProgress);
+        this.color = BABYLON.Color3.Lerp( this.color, new BABYLON.Color3(0, 0, 0), cycleProgress);
+        this.intensity = BABYLON.Scalar.Lerp(this.maxIntesity, 0.01, cycleProgress);
+        console.log(cycleProgress)
         //
         // // Appliquez les changements à la lumière
-        this.lampLight.diffuse = this.color;
-        this.lampLight.intensity = this.intensity;
+        this.directionalLight.diffuse = this.color;
+        this.directionalLight.intensity = this.intensity;
     }
 }
--- src/materials/PavementMaterial.ts
+++ src/materials/PavementMaterial.ts
@@ -5,8 +5,14 @@ export class PavementMaterial extends GOWMaterial{
     constructor(scene :BABYLON.Scene, scale : number ){
         super(scene,"pavement", scale, scale);
 
+        const samplingMode : number = BABYLON.Texture.CUBIC_MODE;
+        const specularPower : number = 1000;
+        const parallaxScaleBias : number = 0.05;
+        const useParallax = true;
+        const useParallaxOcclusion = true;
+
         const diffuse: BABYLON.Texture    =  new BABYLON.Texture(this.diffusePath, scene,
-            false,false, BABYLON.Texture.NEAREST_SAMPLINGMODE);
+            false,false,samplingMode );
         this.diffuseTexture =  diffuse;
         // @ts-ignore
         this.diffuseTexture.uScale = scale;
@@ -14,33 +20,43 @@ export class PavementMaterial extends GOWMaterial{
         this.diffuseTexture.vScale = scale;
 
         this.specularTexture = new BABYLON.Texture(this.ambiantPath,
-            scene,false,false,BABYLON.Texture.NEAREST_SAMPLINGMODE);
+            scene,false,false,samplingMode);
         // @ts-ignore
         this.specularTexture.uScale = scale;
         // @ts-ignore
         this.specularTexture.vScale = scale;
 
-        // material.emissiveTexture = diffuseTexture;
-        // material.emissiveTexture.uScale = scale;
-        // material.emissiveTexture.vScale = scale;
+        this.emissiveTexture = this.diffuseTexture;
+        // @ts-ignore
+        this.emissiveTexture.uScale = scale;
+        // @ts-ignore
+        this.emissiveTexture.vScale = scale;
 
+        // AMBIANT TEXTURE --------
         this.ambientTexture = new BABYLON.Texture(this.ambiantPath, scene,
-            false,false, BABYLON.Texture.NEAREST_SAMPLINGMODE);
+            false,false, samplingMode);
         // @ts-ignore
         this.ambientTexture.uScale = scale;
         // @ts-ignore
         this.ambientTexture.vScale = scale;
 
+
+        // BUMP TEXTURE ----------
         this.bumpTexture = new BABYLON.Texture(this.normalPath,
-            scene,false,false,BABYLON.Texture.NEAREST_SAMPLINGMODE);
+            scene,false,false,samplingMode);
         // @ts-ignore
         this.bumpTexture.uScale = scale;
         // @ts-ignore
         this.bumpTexture.vScale = scale;
-        this.useParallax = true;
-        this.useParallaxOcclusion = true;
-        this.parallaxScaleBias = 0.02;
-        this.specularPower = 100.0;
-        this.specularColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+        this.useParallax = useParallax;
+        this.useParallaxOcclusion = useParallaxOcclusion;
+        this.parallaxScaleBias =parallaxScaleBias;
+        this.specularPower = specularPower;
+        this.specularColor = new BABYLON.Color3(0, 0, 0);
+
+        // this.diffuseTexture.uOffset = 5; // Ajustez la valeur pour la rotation sur l'axe U
+        // this.diffuseTexture.vOffset = 5; // Ajustez la valeur pour la rotation sur l'axe V
+        // this.diffuseTexture.
+
     }
 }
